2000eaea51ea75929247daf2178ab300ce94bb25
dguerrajoselin <dguerrajoselin@gmail.com>
2025-05-17 05:54:05 +0000

    Cambios en estructura de proyecto - eliminacion de carrusel flowbite

--- src/app/sections/main-section/main-section.component.ts
+++ src/app/sections/main-section/main-section.component.ts
@@ -1,10 +1,9 @@
 import { Component, ElementRef, QueryList, signal, ViewChild, ViewChildren, viewChildren } from '@angular/core';
-import { CarrouselComponent } from "./components/carrousel/carrousel.component";
 import { CarrouselOwnComponent } from "./components/carrousel-own/carrousel-own.component";
 
 @Component({
   selector: 'app-main-section',
-  imports: [CarrouselComponent, CarrouselOwnComponent],
+  imports: [ CarrouselOwnComponent],
   templateUrl: './main-section.component.html',
 })
 export class MainSectionComponent {
