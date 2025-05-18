fdb13268e593a1030382a565529fd51056e23ee4
dotDipana <dguerrajoselin@gmail.com>
2025-05-18 00:29:33 -0400

    Cambios finales carrusel

--- src/app/sections/main-section/components/carrousel-own/carrousel-own.component.ts
+++ src/app/sections/main-section/components/carrousel-own/carrousel-own.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, QueryList, Renderer2, signal, ViewChild, ViewChildren } from '@angular/core';
+import { Component, effect, ElementRef, QueryList, Renderer2, signal, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-carrousel-own',
@@ -9,29 +9,62 @@ import { Component, ElementRef, QueryList, Renderer2, signal, ViewChild, ViewChi
 export class CarrouselOwnComponent {
 
 
+  //Array de imagenes de prueba
+  objImages: string[] = ["element1","element2","element3"];
 
   constructor(private renderer:Renderer2){}
-
 
   //Selector de padre
   @ViewChild('carrousel') carrousel!: ElementRef;
 
   //Selector de hijos
   @ViewChildren('slide') slides!: QueryList<ElementRef>;
+  @ViewChildren('navigationButtons') navButttons!: QueryList<ElementRef>;
 
   activePositionIndex = signal<number>(0);
   containerWidth = signal<number>(0);
 
+  //LifeCycle ngAfterView para inicializar las referencias cuando el dom carge
+
+  ngAfterViewInit(){
+    this.renderer.removeClass(this.navButttons.get(0)?.nativeElement,"text-gray-100/70");
+    this.renderer.addClass(this.navButttons.get(0)?.nativeElement,"text-black/70");
+
+    //Api de Js que observa el cambio de un contenedor y no la ventana
+    const resizeObserver = new ResizeObserver(()=>{
+      this.containerWidth.set(this.carrousel.nativeElement.offsetWidth);
+    });
+    resizeObserver.observe(this.carrousel.nativeElement);
+  }
+
+  //TODO: NGONDESTROY por si lo necesito para el resizeobserver
+
+
+  //Change slides
   next(){
-    this.containerWidth.set(this.carrousel.nativeElement.offsetWidth);
     this.activePositionIndex.update(pos =>(pos<this.slides.length-1)? pos + 1 : 0);
     this.renderer.setStyle(this.carrousel.nativeElement,'transform',`translateX(${-this.containerWidth()*this.activePositionIndex()}px)`);
   }
 
   prev(){
-    this.containerWidth.set(this.carrousel.nativeElement.offsetWidth);
     this.activePositionIndex.update(pos =>(pos>0)? pos - 1 : this.slides.length-1);
     this.renderer.setStyle(this.carrousel.nativeElement,'transform',`translateX(${-this.containerWidth()*this.activePositionIndex()}px)`);
   }
 
+  //Ir al slide
+  goToSlide(slidePos: number): void{
+    this.activePositionIndex.set(slidePos);
+    this.renderer.setStyle(this.carrousel.nativeElement,'transform',`translateX(${-this.containerWidth()*this.activePositionIndex()}px)`);
+    this.changeDotColor(slidePos);
+  }
+
+  changeDotColor(pos:number){
+    this.navButttons.forEach((el)=>{
+      this.renderer.removeClass(el.nativeElement,"text-black/70");
+      this.renderer.addClass(el.nativeElement,"text-gray-100/70");
+    })
+
+    this.renderer.removeClass(this.navButttons.get(pos)?.nativeElement,"text-gray-100/70");
+    this.renderer.addClass(this.navButttons.get(pos)?.nativeElement,"text-black/70");
+  }
 }
