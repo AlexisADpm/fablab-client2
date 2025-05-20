54ce9d2ee959a8a02f7a21c0cd1cbd4c7e6c1d39
dguerrajoselin <dguerrajoselin@gmail.com>
2025-05-20 03:37:49 +0000

    cambios en estructura general

---
+++ src/app/sections/main-section/components/for-us-cards/for-us-cards.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CardComponent } from "../../../../shared/card/card.component";
+
+@Component({
+  selector: 'for-us-cards',
+  imports: [CardComponent],
+  templateUrl: './for-us-cards.component.html',
+  styleUrl: './for-us-cards.component.css'
+})
+export class ForUsCardsComponent {
+
+
+}
