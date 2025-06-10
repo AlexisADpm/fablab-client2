b801d1f6fb5866ad11510b9316cce4532a093bd9
AlexisADpm <alexisperez.1120@gmail.com>
2025-06-10 16:11:26 -0400

    Minimal changes

--- src/app/sections/main-section/components/projects/projects.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import dblocalproyectos from './dblocalproyectos.json';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class ProjectsService {
-  constructor() {}
-}
